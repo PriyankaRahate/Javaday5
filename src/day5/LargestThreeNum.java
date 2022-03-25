cf72df2bf3b5b92bb929a757437423195e9967a9
unknown <priyanka.rahate23@gmail.com>
2022-03-25 14:49:23 +0530

    Largestnum

---
+++ src/day5/LargestThreeNum.java
@@ -0,0 +1,24 @@
+package day5;
+
+import java.util.Scanner;
+
+public class LargestThreeNum {
+
+	public static void main(String[] args) {
+     int a,b,c;
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter three number");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+		 
+		if(a>b && a>c)
+			System.out.println(a + " is greater");
+		else if(b>c && b>a)
+			System.out.println(b + " is greater");
+		else
+			System.out.println(c + " is greater");
+	}
+
+}
